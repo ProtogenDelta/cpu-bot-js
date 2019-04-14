eaba49d01c80126f1d1ab41c930aab6f478c080a
braedynscode <47292121+braedynscode@users.noreply.github.com>
2019-04-14 14:19:51 +1000

    Update index.js
    Added Ping command, changed prefix and prefix handler.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log("Loading...");
 
 // Import the discord.js module
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 // Create an instance of Discord that we will use to control the bot
 const bot = new Discord.Client();
 
 // Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
 const token = process.env.token;
+
+//Prefix for your bot, Can be changed to anything.
+const cmdprefix = ">";
 
 console.log("Discord.js Ready!");
 
@@ -20,29 +23,38 @@ bot.on('ready', () => {
 bot.on('message', message => {
     // So the bot doesn't reply to iteself
     if (message.author.bot) return;
-
-    if (message.content === ".hello") {
-        message.channel.send("```Hello!```")
+    if (!message.content.startsWith(cmdprefix)) return;
+    var cmd = message.content.substr(1).trim();
+     
+    if (cmd === "help") {
+        message.channel.send("``` Help \n Prefix is"+cmdprefix+" \n Help - Display this dialog \n Hello - Say Hello! \n 8ball - Make a decision \n Flip - Flip a Coin \n Howsmart - Determines how smart you are. /n Ping - Determine the bot response time. ```");
+        
+     }
+    
+    if (cmd === "ping") {
+        message.channel.send("```Pong! The bot's ping time is "+Math.round(bot.ping)+"ms.```");
     }
     
-    if (message.content === ".8ball") { 
-        var items = Array("Yes","No","Maybe","Definitely","Probably","Probably Not","Try Again")
+    if (cmd === "hello") {
+        message.channel.send("```Hello!```");
+    }
+    
+    if (cmd === "8ball") { 
+        var items = Array("Yes","No","Maybe","Definitely","Probably","Probably Not","Try Again");
         var item = items[Math.floor(Math.random() * items.length)];
-        message.channel.send("```"+item+"```")
+        message.channel.send("```"+item+"```");
     }
 
-    if (message.content === ".flip") {
-        var headstails = ["Heads","Tails"]
+    if (cmd === "flip") {
+        var headstails = ["Heads","Tails"];
         var ht = headstails[Math.floor(Math.random() * headstails.length)];
-        message.channel.send("```"+ht+"```")
+        message.channel.send("```"+ht+"```");
     }
 
-    if (message.content === ".help") {
-        message.channel.send("``` Help \n Prefix is . \n Help - Display this dialog \n Hello - Say Hello! \n 8ball - Make a decision \n Flip - Flip a Coin \n Howsmart - Determines how smart you are. ```")
-    }
-
-    if (message.content.startsWith(".howsmart")) {
-        message.channel.send("```"+message.author.username+" is "+(Math.floor(Math.random()*100)) +"% smart.```")
+    if (cmd.startsWith("howsmart")) {
+        var parts = cmd.split(" ");
+        var name = parts.length > 1 ? parts[1] : message.author.username;
+        message.channel.send("```"+name+" is "+(Math.floor(Math.random()*100)) +"% smart.```");
     }
 });
   
