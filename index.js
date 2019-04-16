ca1092bfce75300d621ca255fab82ba16dffcf6d
braedynscode <47292121+braedynscode@users.noreply.github.com>
2019-04-16 13:13:20 +1000

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ stdin.addListener("data", function(d) {
     // note:  d is an object, and when converted to a string it will
     // end with a linefeed.  so we (rather crudely) account for that  
     // with toString() and then trim() 
-    console.log("you entered: [" + 
-        d.toString().trim() + "]");
+    console.log("you entered: [" + d.toString().trim() + "]");
+    bot.channels.get(567547154005098499).send(d.toString().trim())
   });
 console.log("stdin ready!")
 
